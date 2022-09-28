30f89379cce16cb8d3b3bcc7582d9a97d57c0481
Admin <dinhthaison2000@gmail.com>
2022-09-28 16:29:14 +0700

    code tuan 2

---
+++ src/ChuViTamGiac.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class ChuViTamGiac {
+    public static double tinhTamGiac(Point p1, Point p2, Point p3){
+        double d1 = Math.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y));
+        double d2 = Math.sqrt((p2.x-p3.x)*(p2.x-p3.x) + (p2.y-p3.y)*(p2.y-p3.y));
+        double d3 = Math.sqrt((p1.x-p3.x)*(p1.x-p3.x) + (p1.y-p3.y)*(p1.y-p3.y));
+
+        if (d1 + d2 <= d3 || d2 + d3 <= d1 || d1 + d3 <= d2){
+            return 0;
+        }
+
+        return d1 + d2 + d3;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-->0){
+            double x1 = scanner.nextDouble();
+            double y1 = scanner.nextDouble();
+            double x2 = scanner.nextDouble();
+            double y2 = scanner.nextDouble();
+            double x3 = scanner.nextDouble();
+            double y3 = scanner.nextDouble();
+
+            Point p1 = new Point(x1, y1);
+            Point p2 = new Point(x2, y2);
+            Point p3 = new Point(x3, y3);
+
+            if (tinhTamGiac(p1, p2, p3) == 0){
+                System.out.println("INVALID");
+
+            } else {
+                System.out.println(String.format("%.3f", tinhTamGiac(p1, p2, p3)));
+            }
+        }
+    }
+}
+
+class PointTamGiac{
+    double x;
+    double y;
+
+    public PointTamGiac(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/TuyenDung.java
@@ -0,0 +1,18 @@
+public class TuyenDung {
+}
+
+class ThiSinhTD{
+    private String maTS;
+    private String hoTen;
+    private float diemLT;
+    private float diemTH;
+
+    public ThiSinhTD(String maTS, String hoTen, float diemLT, float diemTH) {
+        this.maTS = maTS;
+        this.hoTen = hoTen;
+        this.diemLT = diemLT;
+        this.diemTH = diemTH;
+    }
+
+
+}
