f2aa9fe8f7475b34e7bcb63ab02f71dbd6ed3576
dinhthaison <dinhthaison2000@gmail.com>
2022-10-07 23:19:32 +0700

    code test t6 tuan 3

---
+++ src/BienDoiSoTuNhien.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class BienDoiSoTuNhien {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-->0){
+            long n = scanner.nextLong();
+            Queue<Long> queue = new LinkedList<>();
+            queue.add(n);
+            Map<Long, Integer> map = new HashMap<>();
+            map.put(n, 0);
+            while (!queue.isEmpty()){
+                long number = queue.poll();
+                int buoc = map.get(number);
+                if (number - 1 == 1){
+                    System.out.println(buoc + 1);
+                    break;
+                }
+                if (number - 1 > 0 && map.get(number-1) == null){
+                    map.put(number-1, buoc+1);
+                    queue.add(number-1);
+                }
+
+                for (int i = 2;i<=Math.sqrt(number);i++){
+                    if (number % i == 0){
+                        if (map.get(number/i) ==  null){
+                            map.put(number/i, buoc+1);
+                            queue.add(number/i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- src/DSA07019.java
+++ src/DSA07019.java
@@ -2,49 +2,35 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class DSA07019 {
-    public static int n;
-    public static int[] arr, L, R;
-
-    static void init() {
-        Stack<Integer> stack = new Stack<Integer>();
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) stack.pop();
-            if (stack.isEmpty()) L[i] = -1;
-            else L[i] = stack.peek();
-            stack.push(i);
-        }
-        while (!stack.isEmpty()) stack.pop();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) stack.pop();
-            if (stack.isEmpty()) R[i] = n;
-            else R[i] = stack.peek();
-            stack.push(i);
-        }
-    }
-
-    static void solve() {
-        long max = 0;
-        for (int i = 0; i < n; ++i) {
-            long t = (long) arr[i] * (R[i] - L[i] - 1);
-            if (max < t) max = t;
-        }
-        System.out.println(max);
-    }
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            n = sc.nextInt();
-            arr = new int[n + 1];
-            for (int i = 0; i < n; i++) {
-                arr[i] = sc.nextInt();
-            }
-            L = new int[100000];
-            R = new int[100000];
-            init();
-            solve();
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
 
+        while (t-->0){
+            int n = scanner.nextInt();
+            int a[] = new int[n];
+
+            for (int i=0;i<n;i++){
+                a[i] = scanner.nextInt();
+            }
+            xuly(a, n);
         }
+    }
+
+    public static void xuly(int a[], int n){
+        int R[] = new int[n], i, j;
+        Stack<Integer> stack = new Stack<>();
+        for (i=n-1;i>=0;i--){
+            while (!stack.empty() && a[i] >= stack.peek()) stack.pop();
+            if (stack.empty()) R[i] = -1;
+            else R[i] = stack.peek();
+            stack.push(a[i]);
+        }
+
+        for (i=0;i<n;i++){
+            System.out.println(R[i] + " ");
+        }
+
+        System.out.println();
     }
 }
